41ba1936810cd97a14a1a187849f0191509498f5
kamrulmiazi <kamrul116522@gmail.com>
2026-03-01 02:14:45 +0600

    update

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -9,7 +9,7 @@ import Link from "next/link";
 import React from "react";
 
 const Login = () => {
-  const handleSubmit = (e) => {
+  const handleSubmit = (e: any) => {
     e.preventDefault();
     const from = e.target;
     const email = from.email.value;
